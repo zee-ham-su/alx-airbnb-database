35f77be336bab5545253ac1109607d348b7f7963
zee-ham-su <hsufiian@yahoo.com>
2024-11-26 18:53:43 +0000

    feat: Add ORDER BY clause to SQL join queries for improved result sorting

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -28,6 +28,8 @@ FROM
     property p
 LEFT JOIN 
     review r ON p.property_id = r.property_id;
+ORDER BY
+    p.property_id;
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
 SELECT 
