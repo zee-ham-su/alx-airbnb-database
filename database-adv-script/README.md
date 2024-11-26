5668030488b923bc1bf186fe84f8fe0a2630d2e0
zee-ham-su <hsufiian@yahoo.com>
2024-11-26 18:50:38 +0000

    feat: Add SQL join queries and documentation for AirBnB clone database

---
+++ database-adv-script/README.md
@@ -0,0 +1,19 @@
+# SQL Joins Queries - AirBnB Clone Database
+
+## Overview
+
+This repository contains SQL queries that demonstrate the use of different types of joins (INNER JOIN, LEFT JOIN, and FULL OUTER JOIN) to retrieve data from the AirBnB Clone database.
+
+## Queries
+
+### 1. INNER JOIN Query
+
+This query retrieves all bookings and the respective users who made those bookings.
+
+### 2. LEFT JOIN Query
+
+This query retrieves all properties and their reviews, including properties that have no reviews.
+
+### 3. FULL OUTER JOIN Query
+
+This query  retrieves all users and all bookings, even if the user has no booking or a booking is not linked to a user.
---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,46 @@
+-- 1. INNER JOIN: Retrieve all bookings and the respective users who made those bookings
+SELECT 
+    b.booking_id, 
+    b.start_date, 
+    b.end_date, 
+    b.total_price, 
+    b.status,
+    u.user_id, 
+    u.first_name, 
+    u.last_name, 
+    u.email
+FROM 
+    booking b
+INNER JOIN 
+    "user" u ON b.user_id = u.user_id;
+
+-- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
+SELECT 
+    p.property_id, 
+    p.name AS property_name, 
+    p.description, 
+    p.location, 
+    p.price_per_night,
+    r.review_id, 
+    r.rating, 
+    r.comment
+FROM 
+    property p
+LEFT JOIN 
+    review r ON p.property_id = r.property_id;
+
+-- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
+SELECT 
+    u.user_id, 
+    u.first_name, 
+    u.last_name, 
+    u.email,
+    b.booking_id, 
+    b.start_date, 
+    b.end_date, 
+    b.total_price, 
+    b.status
+FROM 
+    "user" u
+FULL OUTER JOIN 
+    booking b ON u.user_id = b.user_id;
